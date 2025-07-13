686d25a9ff6db247682fef98733010e63b6a8c83
Apratim <Aapratimboy@example.com>
2025-07-13 09:27:12 +0545

    updated DEBUG & ALLOWED_HOSTS

--- home/settings.py
+++ home/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-06h1#2pfqhdeft30x=#f3rx(@pe7c9js0@90a=6u63o#dodkha'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False  # Set to False for production
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
